bcced729b5d5e6bb16e2ef402818410962f51e9d
SaeedhShareef <67280404+SaeedhShareef@users.noreply.github.com>
2024-06-28 02:08:14 -0400

    updated Navbar for mobile viewing

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ $(document).ready(function(e){
     $win = $(window);
     $navbar = $('#header');
     $toggle = $('.toggle-button');
+    $home = $('.home');
+    $skills = $('.skills');
+    $projects = $('.projects');
+    $education = $('.education');
+    $experience = $('.experience');
+    $resume = $('.resume');
+    $contact = $('.contact');
+   
     var width = $navbar.width();
     toggle_onclick($win, $navbar, width);
     // resize event
@@ -11,9 +19,31 @@ $(document).ready(function(e){
     $toggle.click(function(e){
         $navbar.toggleClass("toggle-left");
     })
+
 });
 function toggle_onclick($win, $navbar, width){
     if($win.width() <= 768){
+        $home.click(function(e){
+            $navbar.toggleClass("toggle-left");
+        })
+        $skills.click(function(e){
+            $navbar.toggleClass("toggle-left");
+        })
+        $projects.click(function(e){
+            $navbar.toggleClass("toggle-left");
+        })
+        $education.click(function(e){
+            $navbar.toggleClass("toggle-left");
+        })
+        $experience.click(function(e){
+            $navbar.toggleClass("toggle-left");
+        })
+        $resume.click(function(e){
+            $navbar.toggleClass("toggle-left");
+        })
+        $contact.click(function(e){
+            $navbar.toggleClass("toggle-left");
+        })
         $navbar.css({left: `-${width}px`});
     }else{
         $navbar.css({left: '0px'});
